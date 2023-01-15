8b51946a5f07a0bae1b06e7a9be1d1f5c603bfd1
Sefdine Nassuf <74672023+Sefdine@users.noreply.github.com>
2023-01-15 10:31:15 +0100

    ajout des menu en utilisant le fichier tsv

--- header.php
+++ header.php
@@ -12,7 +12,7 @@ require_once('functions.php');
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.104.2">
-    <title>Top navbar example · Bootstrap v5.2</title>
+    <title><?= $title ?? 'Notre site' ?></title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
--- menu.php
+++ menu.php
@@ -1,18 +1,31 @@
 <?php
+$title = 'Notre menu';
 include_once('header.php');
 
-$fichier = __DIR__.DIRECTORY_SEPARATOR.'menu.txt';
-$ressource = fopen($fichier, 'r');
-
+$lignes = file(__DIR__.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'menu.txt');
+foreach($lignes as $k => $ligne) {
+    $lignes[$k] = explode("\t", trim($ligne));
+}
 ?>
 
 <div class="container">
-    <?php while($line = fgets($ressource)): ?>
-    <h2><?= $line ?></h2>
-    <ul>
-        <li><?= $line ?></li>
-        <li><?= $line ?></li>
-        <li><?= $line ?></li>
-    </ul>
-    <?php endwhile ?>
+    <?php foreach($lignes as $ligne): ?>
+        <?php if(count($ligne) === 1): ?>
+            <h2><?= $ligne[0] ?></h2>
+        <?php else: ?>
+            <div class="row">
+                <div class="col-sm-8">
+                    <p>
+                        <strong><?= $ligne[0] ?></strong><br>
+                        <?= $ligne[1] ?>
+                    </p>
+                </div>
+                <div class="col-sm-4">
+                    <strong><?= number_format($ligne[2], 2,',') ?>€</strong>
+                </div>
+            </div>
+        <?php endif ?>
+    <?php endforeach ?>
 </div>
+
+<?php require_once('footer.php'); ?>
